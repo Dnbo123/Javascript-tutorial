7770d21d0139fbfb7959832cc2caf5e6b53c0394
Don Bosco <don965877@gmail.com>
2024-09-14 00:15:29 +0300

    deployed local js file to repository

---
+++ JS-Fullcourse/scripts/11-todo-list.js
@@ -0,0 +1,52 @@
+const toDoList = [{ 
+    name: 'Get the Bag',
+    dueDate: '13-9-2024'
+}, { 
+     name: 'Get a new spot',
+     dueDate: '01-10-2025'
+    }];
+renderToDo();
+
+function renderToDo() {
+
+let toDoListHTML = '';
+
+for(let i = 0; i < toDoList.length; i++) {
+    const toDoObject = toDoList[i];
+   // const name = toDoObject.name;
+   // const dueDate = toDoObject.dueDate;
+   const {name, dueDate} = toDoObject;
+    const html = `
+       <div>${name}</div>
+        <div>${dueDate}</div> 
+          <button onClick="
+          toDoList.splice(${i}, 1);
+          renderToDo();
+          ">Delete</button>
+    `;
+    toDoListHTML += html;
+}
+console.log(toDoListHTML);
+
+document.querySelector('.js-list').innerHTML = toDoListHTML;
+}
+
+function addInput() {
+const inputElement = document.querySelector('.js-toDo-input');
+const name = inputElement.value;
+
+const dateInputElement = document.querySelector('.js-due-date-input');
+const dueDate = dateInputElement.value;
+
+console.log(name);
+
+toDoList.push({
+    name,
+    dueDate
+});
+console.log(toDoList);
+
+inputElement.value = '';
+
+renderToDo();
+}
