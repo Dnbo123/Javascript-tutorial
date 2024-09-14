d5f0e96fb5cba7b6026199dcf58d7e44f5dfeff3
Don Bosco <don965877@gmail.com>
2024-09-14 04:49:42 +0300

    updated .forEach in todo-list

---
+++ JS-Fullcourse/scripts/12-todo-list.js
@@ -0,0 +1,48 @@
+const toDoList = [{ 
+    name: 'Get the Bag',
+    dueDate: '13-9-2024'
+}, { 
+     name: 'Get a new spot',
+     dueDate: '01-10-2025'
+    }];
+renderToDo();
+
+function renderToDo() {
+
+let toDoListHTML = '';
+
+toDoList.forEach(function(toDoObject, index) {
+    const {name, dueDate} = toDoObject;
+     const html = `
+        <div>${name}</div>
+         <div>${dueDate}</div> 
+           <button onClick="
+           toDoList.splice(${index}, 1);
+           renderToDo();
+           " class="input-delete">Delete</button>
+     `;
+     toDoListHTML += html;
+});
+
+document.querySelector('.js-list').innerHTML = toDoListHTML;
+}
+
+function addInput() {
+const inputElement = document.querySelector('.js-toDo-input');
+const name = inputElement.value;
+
+const dateInputElement = document.querySelector('.js-due-date-input');
+const dueDate = dateInputElement.value;
+
+console.log(name);
+
+toDoList.push({
+    name,
+    dueDate
+});
+console.log(toDoList);
+
+inputElement.value = '';
+
+renderToDo();
+}
