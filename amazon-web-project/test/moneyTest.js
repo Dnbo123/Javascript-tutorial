2a0ce35b4294d1a726cbff13ae74d724845436c6
Don Bosco <don965877@gmail.com>
2024-11-19 01:54:39 +0300

    Create automated test for formatCurrency

---
+++ amazon-web-project/test/moneyTest.js
@@ -0,0 +1,34 @@
+import formatCurrency from "../scripts/utilities/money.js";
+
+
+//FormatCurrency test 
+
+//Grouping the tests
+console.log('Test suite: FormatCurrency');
+//(1) Basic test cases
+
+if (formatCurrency(2095) === '20.95') {
+    console.log('passed');
+} else {
+    console.log('failed');
+}
+
+
+//(2) Edge test Cases
+if(formatCurrency(0) === '0.00') {
+     console.log('Yeah');
+} else {
+    console.log('Nope');
+}
+
+if(formatCurrency(2000.5) === '20.01') {
+    console.log('Yeah');
+} else {
+   console.log('Nope');
+}
+
+if(formatCurrency(2000.4) === '20.00') {
+    console.log('Yeah');
+} else {
+   console.log('Nope');
+}
