763a6e9dcc4a82278e7ea8c0479d399b3172cb9f
Don Bosco <don965877@gmail.com>
2024-12-08 04:47:56 +0300

    convert products into classes

--- amazon-web-project/scripts/checkOut/orderSummary.js
+++ amazon-web-project/scripts/checkOut/orderSummary.js
@@ -40,7 +40,7 @@ cart.forEach((cartItem) => {
                   ${matchingProduct.name}
                 </div>
                 <div class="product-price">
-              $${formatCurrency(matchingProduct.priceCents)}
+              $${matchingProduct.productPrice()}
                 </div>
                 <div class="product-quantity 
                 js-product-quantity-${matchingProduct.id}">
--- amazon-web-project/scripts/checkout.js
+++ amazon-web-project/scripts/checkout.js
@@ -1,7 +1,7 @@
 import { renderCheckOutHeader } from './checkOut/checkOutHeader.js';
 import {renderOrderSummary} from './checkOut/orderSummary.js';
 import {renderPaymentSummary} from './checkOut/paymentSummary.js'
-import '../data/cart-class.js';
+//import '../data/cart-class.js';
 
 renderCheckOutHeader();
 renderOrderSummary();
